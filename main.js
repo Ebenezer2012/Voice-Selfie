bb9620a4292cbf0ede6a67425e9ab0d34f25c1c2
Ebenezer2012 <89516748+Ebenezer2012@users.noreply.github.com>
2022-01-10 17:33:40 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,21 @@ console.log (event) ;
 var Content = event.results[0][0].transcript;
 console.log (Content);
 document.getElementById("textbox").innerHTML = Content;
+speak();
 }
+function speak(){
+var symth=window.speechSynthesis;
+speak_data=document.getElementById("textbox").value;
+var uttterThis=new SpeechSynthesisUtterance(speak_data);
+symth.speak(uttterThis);
+Webcam.attach(camera);
+
+}
+
+Webcam.set({
+width:360,
+height:250,
+image_format:'png',
+png_quality:90
+})   ;
+camera=document.getElementById("camera") ;
